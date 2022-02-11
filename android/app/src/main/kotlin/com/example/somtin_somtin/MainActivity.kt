aad6610078ddb09ad8e209df5e68c09621640af5
YawAntwiOwusu <me@yawantwiowusu.com>
2022-02-11 13:21:06 +0000

    create a new project

---
+++ android/app/src/main/kotlin/com/example/somtin_somtin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.somtin_somtin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:somtin_somtin/myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
